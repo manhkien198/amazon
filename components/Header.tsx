12dd65565aa3fd8b93be098c862101e798189bb6
manhkien2521998 <kienpv@xproz.com>
2023-01-11 23:14:27 +0700

    update header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Logo from '../assets/images/logo.png';
 import Image from 'next/image';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
 export default function Header() {
   return (
     <header>
       {/* top nav */}
-      <div className='py-2 bg-[#131921] flex items-center flex-grow'>
-        <div className='flex mt-2 flex-grow sm:flex-grow-0 items-center'>
+      <div className='py-2 bg-[#131921] flex items-center grow'>
+        <div className='flex mt-2 sm:grow-0 md:grow items-center'>
           <Image
             src={Logo}
             alt='amazon logo'
@@ -15,6 +17,14 @@ export default function Header() {
             objectFit='contain'
             className='cursor-pointer'
           />
+        </div>
+        {/* search */}
+        <div className='bg-yellow-400 hover:bg-yellow-500 sm:flex items-center h-10 rounded-md grow cursor-pointer'>
+          <input
+            type='text'
+            className='h-full p-2 grow shrink rounded-l-md focus:outline-none'
+          />
+          <MagnifyingGlassIcon className='h-12 p-4' />
         </div>
       </div>
       {/* bottom nav */}
